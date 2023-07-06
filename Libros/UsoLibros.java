c54a4b361035d0e74c3565feb173782923b257dc
Lucassarfati1 <60708040+Lucassarfati1@users.noreply.github.com>
2023-07-06 00:33:15 -0300

    Update UsoLibros.java

--- Libros/UsoLibros.java
+++ Libros/UsoLibros.java
@@ -9,8 +9,8 @@ public class UsoLibros {
 		Libro elPoderDelAhora=new Libro("El poder del ahora","Ekhart Tolle",978034073);
 		Libro liberados=new Libro("Liberados","Woolverton Frederick",978607735);
 		Libro anaFrank=new Libro("El diario de Ana Frank","La editorial",978000010);
-		String baka="pene";
-		System.out.println("pene".equals(baka));
+		String baka="tt";
+		System.out.println("tt".equals(baka));
 		
 	}
 
